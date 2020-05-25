4185d00af91140a0e3f8cd09f226ab113256c655
silvioaburto <sjortiz@ucdavis.edu>
2020-05-25 13:21:17 -0700

    update app

--- public/loteria/functions.js
+++ public/loteria/functions.js
@@ -6,8 +6,14 @@ var timer = document.getElementById("timer");
 var card_index_div = document.getElementById("card_index_div");
 var history_element = document.getElementById(`history_div`);
 var used_cards_div = document.getElementById(`used_cards`);
-var deck_id = document.getElementById(`used_cards`);
-var elem = document.getElementById("progress_bar");
+var deck_id_div = document.getElementById(`deck_id`);
+var pause_icon =   document.getElementById(`pause_icon`);
+var new_game = document.getElementById(`new_game`)
+var exit_icon_history =   document.getElementById(`exit_icon_history`)
+var exit_icon_config =   document.getElementById(`exit_icon_config`)
+var left_icon = document.getElementById(`left_arrow_icon`)
+var right_icon = document.getElementById(`right_arrow_icon`)
+var config_div = document.getElementById(`config_div`)
 var images = returnCards();
 var images2 = images;
 var loaderId;
@@ -16,36 +22,33 @@ var width=0;
 var isPlaying = false;
 var imagesUsed = [];
 var HistoryCardNegIndex = 0;
-var change_time = 3500; //in milliseconds 3500 is a good speed
+var change_time = 3000; //in milliseconds 3500 is a good speed
 var img_go_to;
 var img_id;
+var counter = 1;
+var tutorial_up = true;
+var config_up = false;
+var history_up = false;
 
 
 function Deck() {
 }
 
 Deck.prototype.initialise = function() {
+  //getScreenSize();
   this.toggleTutorialButtons();
 };
 
 
-function UpdateButton(){
-  if(isPlaying){
-    start_button.innerHTML = "PLAY"
-  }
-  else {
-    start_button.innerHTML = "PAUSE"
-  }
+function UpdateStatus(){
   isPlaying = !isPlaying;
 }
 
-let counter = 1;
 Deck.prototype.toggleTutorialButtons = function() {
 
-  
   document.getElementById("skipButton").onclick = () => {
     document.getElementById("tutorial").style.display = "none";
-    //this.toggleButtons();
+    tutorial_up = false;
   }
 
   if (document.getElementById("nextButton")) {
@@ -92,6 +95,7 @@ Deck.prototype.toggleTutorialButtons = function() {
         document.getElementById("tutorial").style.display = "none";
         //board.toggleButtons();
       }
+      tutorial_up = false;
     }
   }
 
@@ -101,48 +105,63 @@ Deck.prototype.toggleTutorialButtons = function() {
 function changeCard(){
   images2 = utils.filterUsedCards(images2, imagesUsed); //Remove used images
   if(card_index === 0){
+    document.getElementById(`card_cover`).style.visibility = 'hidden';
     images2 = utils.shuffle(images2)
   }
   card_index_div.textContent = (card_index+1) + "/" + images.length;
-  //console.log("'public/images/" + images2[0].src +".jpg'")
   document.getElementById(`deck_id`).style.backgroundImage = "url('http://silvioaburto.github.io/loteria/public/images/" + images2[0].src +".jpg')";
   utils.playSound(images2[0].name); 
   imagesUsed.push(images2[0].id);
   card_index = card_index + 1;
 }
 
-//Try nested setinterval for more precise timing. 
+
+
+function showHidePauseIcon() {
+  pause_icon.style.visibility = "visible";
+  setTimeout(() => pause_icon.style.visibility = "hidden", 100)
+
+}
+
+//Try a nested setinterval for more precise timing. 
 function PlayCards() {
-  console.log(imagesUsed.length)
-    if(isPlaying){
-      var increment = 10
-      var loader_time = change_time/(100/increment)
+  if(isPlaying){
+      console.log("playing game")
+      console.log("Card Index" + card_index)
+      document.getElementById("play_icon").style.visibility = "hidden";
+      showHidePauseIcon();
+      var increment = 1;
+      var loader_time = change_time/(100/increment);
+      console.log("Loader Time" + loader_time)
+      var progress = $('progress');
       loaderId = setTimeout(function run() {
         if(imagesUsed.length == 54){
           pauseGame();
         }
         else{
-          elem.style.width = width + '%';
-          width= width+increment;
-          if (width>=110){
-            width= 0
-            elem.style.width ='0%';
-            if(imagesUsed ==0){
+          width= progress.val()+increment;
+          progress.val(width);
+          if (width > 100){
+            width = 0
+            if(imagesUsed===0){
               document.getElementById(`card_cover`).style.visibility = 'hidden';
             }
             changeCard();
+            progress.val(progress.attr('min'))
           }
           loaderId = setTimeout(run, loader_time)
         }
       },loader_time);
     } else {
+      document.getElementById("play_icon").style.visibility = "visible";
       pauseGame();
     }
 
 };
 
 function ExitHistory(){
-  pauseGame();
+  //pauseGame();
+  history_up = false;
   document.getElementById(`history_div`).style.visibility = 'hidden';
   document.getElementById(`deck_id`).style.visibility = 'visible';
   PlayCards();
@@ -180,46 +199,68 @@ function historyImgChange(HistoryCardNegIndex){
 }
 
 
-function ShowHistory(){
+
+
+function CloseConfig(){
+  config_div.style.visibility = 'hidden';
+  document.getElementById(`deck_id`).style.visibility = 'visible';
+  right_icon.style.visibility = 'visible';
+  left_icon.style.visibility = 'visible';
+  config_up = false;
+  PlayCards();
+}
+
+
+function OpenConfig(){
+  config_up = true;
   pauseGame();
+  document.getElementById(`play_icon`).style.visibility = "hidden";
+  document.getElementById(`deck_id`).style.visibility = 'hidden';
+  history_element.style.visibility = 'hidden'
+  right_icon.style.visibility = "hidden";
+  left_icon.style.visibility = 'hidden';
+  config_div.style.visibility = 'visible'
+}
+
+
+function ShowHistory(){
+  history_up = true;
+  pauseGame();
+  document.getElementById("play_icon").style.visibility = "hidden";
   HistoryCardNegIndex = 0; //reset history index
-  //console.log(imagesUsed.length)
   document.getElementById(`deck_id`).style.visibility = 'hidden'
   //If the game has lesss than 2 cards do not allow left or right navigation
   history_element.style.visibility = 'visible'
-  history_right.style[`pointer-events`] = 'none'
-  history_right.style.backgroundColor = 'lightgray'
+  right_icon.style[`pointer-events`] = 'none'
+  right_icon.style.color = 'lightgray'
   if(imagesUsed.length < 2) {
-    history_left.style[`pointer-events`] = 'none'
-    history_left.style.backgroundColor = 'lightgray'
+    left_icon.style[`pointer-events`] = 'none'
+    left_icon.style.color = 'lightgray'
   }
   else{
-    history_left.style[`pointer-events`] = 'auto'
-    history_left.style.backgroundColor = 'white'
+    left_icon.style[`pointer-events`] = 'auto'
+    left_icon.style.color = 'lightblue'
   }
   historyImgChange(HistoryCardNegIndex)
- // history_element.textContent = card_index + "/" + images.length
-  //document.getElementById(`deck_id`).className = 'history_id'
+
 }
 
 
 function HistoryBack(){
   HistoryCardNegIndex = HistoryCardNegIndex + 1 
-  //console.log(imagesUsed.length)
-  //console.log(HistoryCardNegIndex)
   if(HistoryCardNegIndex >= 0){
-    history_right.style[`pointer-events`] = 'auto'
-    document.getElementById(`history_right`).style.backgroundColor = 'white'
+    right_icon.style[`pointer-events`] = 'auto'
+    right_icon.style.color = 'lightblue'
   }
   if(HistoryCardNegIndex <= 0){
     // console.log('turn off right')
-     history_right.style[`pointer-events`] = 'none'
-     document.getElementById(`history_right`).style.backgroundColor = 'lightgray'
+     right_icon.style[`pointer-events`] = 'none'
+     right_icon.style.color = 'lightgray'
    }
   if(HistoryCardNegIndex >= imagesUsed.length-1){
     //console.log('turn off left')
-    history_left.style[`pointer-events`] = 'none'
-    document.getElementById(`history_left`).style.backgroundColor = 'lightgray'
+    left_icon.style[`pointer-events`] = 'none'
+    left_icon.style.color = 'lightgray'
   }
   historyImgChange(HistoryCardNegIndex)
 }
@@ -229,51 +270,106 @@ function HistoryNext(){
   HistoryCardNegIndex = HistoryCardNegIndex - 1
   if(HistoryCardNegIndex <= 0){
    // console.log('turn off right')
-    history_right.style[`pointer-events`] = 'none'
-    document.getElementById(`history_right`).style.backgroundColor = 'lightgray'
+    right_icon.style[`pointer-events`] = 'none'
+    right_icon.style.color = 'lightgray'
   }
   if(HistoryCardNegIndex < imagesUsed.length){
     //console.log('turn on left')
-    history_left.style[`pointer-events`] = 'auto'
-    document.getElementById(`history_left`).style.backgroundColor = 'white'
+    left_icon.style[`pointer-events`] = 'auto'
+    left_icon.style.color = 'lightblue'
   }
   historyImgChange(HistoryCardNegIndex)
 }
-
-start_button.addEventListener("click", function() {
-  document.getElementById("tutorial").style.display = "none";
-  utils.LoadSounds(images);
-  UpdateButton();
-  PlayCards();
-}, false);
 
 
 function pauseGame() {
   clearTimeout(loaderId);
 }
 
-
 function resetGame() {
-  clearTimeout(loaderId);
-  imagesUsed = []; //reset imagesUsed to none
-  images = utils.shuffle(images) //reshuffle images
-  card_index= 0; //reset card position
-  HistoryCardNegIndex = 0; //reset history index
-  width=0; //loading bar width
-  elem.style.width =width + '%'; //reset loading bar
-  card_index_div.textContent = card_index + "/" + images.length; //reset card index text
-  document.getElementById(`card_cover`).style.visibility = 'visible';
-  document.getElementById(`deck_id`).style.backgroundImage = 'none';
-  if(isPlaying){
-    UpdateButton();
+  if(confirm("Are you sure?")){
+    var progress = $('progress')
+    clearTimeout(loaderId);
+    imagesUsed = []; //reset imagesUsed to none
+    images = utils.shuffle(images) //reshuffle images
+    card_index= 0; //reset card position
+    HistoryCardNegIndex = 0; //reset history index
+    progress.val(progress.attr('min')) //reset loading bar
+    card_index_div.textContent = card_index + "/" + images.length; //reset card index text
+    config_div.style.visibility = 'hidden';
+    document.getElementById(`deck_id`).style.visibility = 'visible';
+    document.getElementById(`card_cover`).style.visibility = 'visible';
+    right_icon.style.visibility = 'visible';
+    left_icon.style.visibility = 'visible';
+
+    config_up = false;
+    if(isPlaying){
+      UpdateStatus();
+    }
   }
+  else{return;}
 }
 
 
-reset_button.addEventListener("click", resetGame, false)
-history_button.addEventListener("click", ShowHistory, false)
-history_left.addEventListener("click", HistoryBack, false)
-history_right.addEventListener("click", HistoryNext, false)
-history_exit.addEventListener("click", ExitHistory, false)
+function handleCardEvent(){
+  var event_type = event.target.id
+  if(tutorial_up === false){
+    if(event_type === 'deck_id' | event_type === 'play_icon'){
+      document.getElementById("tutorial").style.display = "none";
+      utils.LoadSounds(images);
+      UpdateStatus();
+      PlayCards();
+    }
+    else if(event_type === 'history_icon'){
+      ShowHistory()
+    }
+    else if (event_type === 'config_icon'){
+      OpenConfig()
+    }
+    else{return}
+  }
+  else{return;}
+}
+
+function handleKeyEvent(event){
+  var key_id = event.key
+  if(tutorial_up === false & config_up === false & history_up === false){
+    if(key_id === " "){
+      document.getElementById("tutorial").style.display = "none";
+      utils.LoadSounds(images);
+      UpdateStatus();
+      PlayCards();
+    }
+  }
+  else{return;}
+}
+
+var radioButtons = document.radioForm.speed;
+var prev = null;
+
+function changeSpeed(){
+  if(this !== prev){
+    prev = this
+  }
+  change_time = prev.value*1000;
+  console.log(prev.value)
+
+}
+
+for(var i=0; i < radioButtons.length; i++){
+  radioButtons[i].addEventListener("change",changeSpeed, false)
+}
+
+
+window.addEventListener("keydown", (event) => {handleKeyEvent(event)}, false);
+new_game.addEventListener("click", resetGame, false);
+deck_id_div.addEventListener("click", handleCardEvent, false);
+exit_icon_config.addEventListener("click", CloseConfig, false);
+//reset_button.addEventListener("click", resetGame, false)
+//history_button.addEventListener("click", ShowHistory, false)
+left_icon.addEventListener("click", HistoryBack, false)
+right_icon.addEventListener("click", HistoryNext, false)
+exit_icon_history.addEventListener("click", ExitHistory, false)
+
 let newDeck = new Deck()
 newDeck.initialise();
